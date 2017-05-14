da870d9bb2008e91c14a447c0d512acc953b566c
IhsanTang <IhsanTang@163.com>
2017-05-14 18:35:56 +0800

    发布至api2调试

--- chrome/source/plug-function.js
+++ chrome/source/plug-function.js
@@ -1,4 +1,4 @@
-var websiteUrl = "http://localhost:8080/api";
+var websiteUrl = "http://api2.crap.cn";
 var paramsTr = "<tr class='last'>";
 paramsTr += "<td><input type='text' class='form-control' data-stage='key'></td>";
 paramsTr += "<td><input type='text' class='form-control' data-stage='value'></td>";
